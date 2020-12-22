1b8b71e41321e08f6b4f75f415d324aafa633799
sree <sreelu.ph@gmail.com>
2020-12-22 14:48:11 -0700

    Started skeleton

---
+++ src/components/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="footcontainer">
+        <div id="footdivone">
+            <h2 id="footh2one">Contact</h2>
+            <div id="footcontent">
+                
+
+            </div>
+            
+
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"foot-page"
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+#footcontainer{
+    #footdivone{
+        width: 90vw;
+        min-height:30vh;
+        background-color: white;
+        margin-top:4vh;
+        margin-left: 2vw;
+        border-radius: 5px;
+        #footh2one{
+            text-align: center;
+
+        }
+    }
+
+}
+
+</style>
